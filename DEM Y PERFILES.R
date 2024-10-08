7762ca773ffeecdefa2ddf7b305c8ba091646e4b
PabloChile2022 <Floyd2001ster@gmail.com>
2024-10-08 14:38:57 -0300

    Se agrega funciones para ubicación del predio

--- DEM Y PERFILES.R
+++ DEM Y PERFILES.R
@@ -96,17 +96,6 @@ dist3<-head(c(0,geosphere::distGeo(P3)),-1)
 dist4<-head(c(0,geosphere::distGeo(P4)),-1)
 dist5<-head(c(0,geosphere::distGeo(P5)),-1)
 
-write.table(Coord_P1 %>% mutate(Distancia=dist1), file = "C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Perfil1.txt", sep = ",", quote = FALSE, row.names = FALSE)
-write.table(Coord_P2 %>% mutate(Distancia=dist2), file = "C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Perfil2.txt", sep = ",", quote = FALSE, row.names = FALSE)
-write.table(Coord_P3 %>% mutate(Distancia=dist3), file = "C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Perfil3.txt", sep = ",", quote = FALSE, row.names = FALSE)
-write.table(Coord_P4 %>% mutate(Distancia=dist4), file = "C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Perfil4.txt", sep = ",", quote = FALSE, row.names = FALSE)
-write.table(Coord_P5 %>% mutate(Distancia=dist5), file = "C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Perfil5.txt", sep = ",", quote = FALSE, row.names = FALSE)
-
-writeVector(PLOT_P1,"C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Puntos1.shp", overwrite=TRUE)
-writeVector(PLOT_P2,"C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Puntos2.shp", overwrite=TRUE)
-writeVector(PLOT_P3,"C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Puntos3.shp", overwrite=TRUE)
-writeVector(PLOT_P4,"C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Puntos4.shp", overwrite=TRUE)
-writeVector(PLOT_P5,"C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Puntos5.shp", overwrite=TRUE)
 
 tmap_mode("view")
 
@@ -119,6 +108,133 @@ tm_basemap("Esri.WorldImagery")+
   tm_dots(shape = 24, col = "purple", size = 0.5, fill="purple")+
   tm_shape(puntos_muestreados4)+
   tm_dots(shape = 24, col = "blue", size = 0.5, fill="blue")+
- tm_shape(puntos_muestreados5)+
- tm_dots(shape = 24, col = "orange", size = 0.5, fill="orange")
+  tm_shape(puntos_muestreados5)+
+  tm_dots(shape = 24, col = "orange", size = 0.5, fill="orange")
+
+write.table(Coord_P1 %>% mutate(Distancia=dist1), file = "C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Perfil1.txt", sep = ",", quote = FALSE, row.names = FALSE)
+write.table(Coord_P2 %>% mutate(Distancia=dist2), file = "C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Perfil2.txt", sep = ",", quote = FALSE, row.names = FALSE)
+write.table(Coord_P3 %>% mutate(Distancia=dist3), file = "C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Perfil3.txt", sep = ",", quote = FALSE, row.names = FALSE)
+write.table(Coord_P4 %>% mutate(Distancia=dist4), file = "C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Perfil4.txt", sep = ",", quote = FALSE, row.names = FALSE)
+write.table(Coord_P5 %>% mutate(Distancia=dist5), file = "C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Perfil5.txt", sep = ",", quote = FALSE, row.names = FALSE)
+
+writeVector(PLOT_P1,"C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Puntos1.shp", overwrite=TRUE)
+writeVector(PLOT_P2,"C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Puntos2.shp", overwrite=TRUE)
+writeVector(PLOT_P3,"C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Puntos3.shp", overwrite=TRUE)
+writeVector(PLOT_P4,"C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Puntos4.shp", overwrite=TRUE)
+writeVector(PLOT_P5,"C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Sembrador Capital/Pan de Azucar/Puntos5.shp", overwrite=TRUE)
+
+
+##Ubicación_region
+
+Mapa_regiones<-vect("C:/Users/Pablo/Desktop/Procesamiento/Capas shp/Regiones/Regiones_UTM.shp")
+Mapa_regiones<-st_as_sf(Mapa_regiones)
+
+# Crear una variable que indique si es la "Región de Antofagasta"
+Mapa_regiones$Highlight <- ifelse(Mapa_regiones$Region == "Región de Valparaíso", "Región de Valparaíso", NA)
+
+tmap_mode("plot")
+
+# Plotear con tmap
+Regiones<-tm_shape(Mapa_regiones) +
+  tm_polygons("Highlight", 
+              palette = c("Región de Valparaíso" = "yellow", "NA" = "grey"), 
+              title = "Región") +
+  tm_grid(lines = TRUE, labels.size = 1.5,labels.fontface = "bold", crs = "epsg:32719", labels.show=c(0,1)) +  # Agregar grilla en UTM
+  tm_compass(position = c("left", "top"),size=1) +  # Agregar la rosa de los vientos en la esquina superior izquierda
+  tm_layout(legend.position = c("left", "bottom"),
+            fontface = "bold",
+            legend.title.size = 2,  # Ajustar el tamaño del título
+            legend.title.color = "black",  # Cambiar el color del título de la leyenda a blanco
+            legend.title.fontface = "bold",
+            legend.text.color = "black",
+            legend.text.size = 2,
+            legend.text.fontface = "bold")
+
+##Ubicación_Comuna
+
+Mapa_Comuna<-vect("C:/Users/Pablo/Desktop/Procesamiento/Capas shp/Comunas/COMUNAS_UTM.shp")
+Mapa_Comuna<-st_as_sf(Mapa_Comuna)
+Mapa_Comuna<-Mapa_Comuna %>% filter(Region=="Región de Valparaíso")
+
+##Puntos
+
+Puntos<-vect("C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Octubre/Aquist/SHAPE/Ubicación.shp")
+Puntos<-st_as_sf(Puntos)
+Puntos<-Puntos[,-c(2:11)]
+colnames(Puntos)<-c("id", "geometry")
+
+Vect<-c("Cuncumén", "L1 y L2", "L3 y L4")
+Puntos$id<-Vect
+
+Cuncumén<-Puntos %>% filter(id=="Cuncumén")
+Proyecto<-Puntos[c(2,3),]
+
+
+# Crear una variable que indique si es la "Región de Antofagasta"
+Mapa_Comuna$Highlight <- ifelse(Mapa_Comuna$Comuna == "San Antonio", "San Antonio", NA)
+
+# Plotear con tmap
+Comuna<-tm_shape(Mapa_Comuna) +
+  tm_polygons("Highlight", 
+              palette = c("San Antonio" = "yellow", "NA" = "grey"), 
+              title = "Comuna")+
+  tm_grid(lines = TRUE, labels.size = 0.8,labels.fontface = "bold", crs = "epsg:32719") +  # Agregar grilla en UTM
+  tm_compass(position = c("left", "top"), size=1) +  # Agregar la rosa de los vientos en la esquina superior izquierda
+  tm_layout(legend.position = c("right", "top"),
+            axis.text.size = 0.5, 
+            fontface = "bold",           
+            legend.title.size = 0.5,  # Ajustar el tamaño del título
+            legend.title.color = "black",  # Cambiar el color del título de la leyenda a blanco
+            legend.title.fontface = "bold",
+            legend.text.color = "black",
+            legend.text.size = 0.5,
+            legend.text.fontface = "bold",
+            axis.x.show = FALSE)
+
+
+Ubica<-tm_shape(Mapa_Comuna %>% filter(Comuna=="San Antonio")) +
+  tm_polygons("Highlight", 
+              palette = c("San Antonio" = "yellow", "NA" = "grey"), 
+              title = "Comuna")+  
+  tm_grid(lines = TRUE, labels.size = 1.5,labels.fontface = "bold", crs = "epsg:32719") +  # Agregar grilla en UTM
+  tm_shape(Cuncumén)+
+  tm_dots(shape = 24, col = "red", size = 1, fill="red") +
+  tm_text("id", size = 2, col ="black", ymod = 1,xmod=0, fontface = "bold", NA.show = FALSE)+  # Mostrar texto
+  tm_shape(Proyecto)+
+  tm_dots(shape = 24, col = "red", size = 1, fill="red") +
+  tm_text("id", size = 2, col ="black", ymod = 1,xmod=0, fontface = "bold", NA.show = FALSE)+  # Mostrar texto
+  tm_layout(legend.position = c("left", "bottom"),
+            legend.title.size = 2,  # Ajustar el tamaño del título
+            legend.title.color = "black",  # Cambiar el color del título de la leyenda a blanco
+            legend.title.fontface = "bold",
+            legend.text.color = "black",
+            legend.text.size = 2,
+            legend.text.fontface = "bold",
+            axis.x.show = FALSE)
+
+Ubica
+
+jpeg(filename="C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Octubre/Aquist/PRODUCTOS/Regiones.jpg",width=7480,height=6000,units="px",res=1000)
+
+Regiones
+
+dev.off()
+
+jpeg(filename="C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Octubre/Aquist/PRODUCTOS/Comuna.jpg",width=2000,height=3000,units="px",res=1000)
+
+Comuna
+
+dev.off()
+
+jpeg(filename="C:/Users/Pablo/Desktop/Pablo/FIA/Pozos/2024/Octubre/Aquist/PRODUCTOS/Ubicación.jpg",width=7480,height=6000,units="px",res=1000)
+
+Ubica
+
+dev.off()
+
+
+
+
+
+
 
